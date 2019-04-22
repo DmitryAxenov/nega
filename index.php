0c918891f3d2ce8060bacbeb7804d78525750937
DESKTOP-8U7I64E\Lex <kptlex@gmail.com>
2019-04-22 20:26:19 +0300

    три способа использования стилей

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Фитнес клуб Нега</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+    .color-red {
+        color:red;
+    }
+</style>
+<body>
+<h1 style="color: green">Добро пожаловать на сайт</h1>
+<h1 class="color-red">Добро пожаловать на сайт</h1>
+<h1 class="color-blue">Добро пожаловать на сайт</h1>
+</body>
+</html>
