6589cc6d34e1a1d274e02452f8455bd434c910de
DESKTOP-8U7I64E\Lex <kptlex@gmail.com>
2019-04-23 21:57:56 +0300

    Сделал новости

--- index.php
+++ index.php
@@ -3,22 +3,59 @@
 <head>
     <title>Фитнес клуб Нега</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 <div class="alert alert-primary" role="alert">
     Сайт находится в разработке
 </div>
 <div class="container">
-
-    <h1 class="text-center">Добро пожаловать!</h1>
+    <h1 class="text-center">Новости</h1>
+    <div class="row">
+        <div class="col">
+            <a href="http://nega.loc/massage.php">
+                <img src="https://citifox.ru/wp-content/uploads/2018/10/image-5.jpg" class="img">Появился массаж</a>
+        </div>
+        <div class="col">
+            <a href="http://nega.loc/trening_group.php">
+                <img src="https://avatars.mds.yandex.net/get-pdb/33827/d329c302-32a2-4cf4-9540-baad6b5dc739/s1200"
+                     class="img">Групповые тренировки</a>
+        </div>
+        <div class="col">
+            <a href="http://nega.loc/track.php">
+                <img src="https://ostrovsporta.ru/image/cache/catalog/begovye-dorogki-folder/2/begovaya-dorogka-victoryfit-gym-898-1-800x800.jpg"
+                     class="img">Дорожка снова в деле</a>
+        </div>
+    </div>
+    <div class="row">
+        <a href="http://nega.loc/news.php" class="mx-auto"> <button type="button" class="btn btn-outline-success">Добавить новость</button></a>
+    </div>
+    <?php
+    /**
+     * агая понячл яп онял что ты понял
+     * просто надо центровать верхний по иерарххии элемент
+     * кнопку стоит по центру ссылки, а вот ссылка не поцентру блока
+     */
+    ?>
 </div>
 
 
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
+<?php
+/**
+ * Работает
+ * так мне по центру надо
+ *
+ */
